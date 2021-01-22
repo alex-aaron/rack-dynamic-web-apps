070d9572b2e8ce42cc39229a0c02530c19c66edf
Alex <alexfrancisaaron@gmail.com>
2021-01-22 10:03:04 -0600

    commit tree c7f2c9c1e6a8ad2847f4292eff30dedf28dfbe11

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,13 @@ class Application
   def call(env)
     resp = Rack::Response.new
     resp.write "Hello, World"
+    time_in_day = rand(1..24)
+
+    if time_in_day >= 12
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end
     resp.finish
   end
 
